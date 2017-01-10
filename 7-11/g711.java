bd69d614223e77ff21ce21e1c457dc79bb59fcb9
Huiyu Jia <jiahuiyu0328@gmail.com>
2017-01-11 12:25:44 +1300

    Add files via upload

--- 7-11/g711.java
+++ 7-11/g711.java
@@ -1,5 +1,10 @@
 import java.text.DecimalFormat;
 import java.util.*;
+
+/**
+ * 
+ * @author Huiyu Jia 
+ */
 public class g711{
     private static int g711= 711;
     private static int cents= g711*1000000;
@@ -10,13 +15,13 @@ public class g711{
         for(int i=100; i< 999 ; i++){
             g711=i;
             countSolutions();
-       
+            
         }
         for(int i=0; i<900;i++){
-          if(solutionCount[i]==1){
-            g711=i+100;
-            printSolution();
-          }
+            if(solutionCount[i]==1){
+                g711=i+100;
+                printSolution();
+            }
         }
         System.out.println(solutionCount[889]);
         final long endTime = System.currentTimeMillis();
@@ -43,7 +48,7 @@ public class g711{
         }
     }
     public static void printSolution(){
-         DecimalFormat df = new DecimalFormat("0.00");
+        DecimalFormat df = new DecimalFormat("0.00");
         List<Integer> list = findDivs();
         int size= list.size();
         for (int ai=0; ai<size; ai++) {
@@ -55,18 +60,18 @@ public class g711{
                     d = g711-a-b-c;
                     if( d>=c){
                         if (a*b*c*d==cents && a+b+c+d==g711 ){
-                             System.out.println("$"+df.format((double)g711/100)+" = $" +
-                                                       df.format((double)a/100) + " + $" +
-                                                       df.format((double)b/100)+ " + $"+
-                                                       df.format((double)c/100) +" + $"+
-                                                       df.format((double)d/100));
-                                }
+                            System.out.println("$"+df.format((double)g711/100)+" = $" +
+                                               df.format((double)a/100) + " + $" +
+                                               df.format((double)b/100)+ " + $"+
+                                               df.format((double)c/100) +" + $"+
+                                               df.format((double)d/100));
                         }
                     }
                 }
             }
         }
-        
+    }
+    
     
     
     public static List<Integer> findDivs(){
